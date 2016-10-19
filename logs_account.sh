4e106840c9afc074d00982c50b81055a77438e33
dinghao <18001326539@163.com>
2016-10-19 17:40:15 +0800

    日志统计合并

---
+++ logs_account.sh
@@ -0,0 +1,76 @@
+#!/bin/bash 
+
+which mysql
+
+#MYSQL1="mysql -hmysql.cim.in -uom -pcim120_CIM120 om --default-character-set=utf8 -A -N"
+MYSQL1="mysql -h192.168.3.102 -udh -pgitLgit dinghaot1 --default-character-set=utf8 -A -N"
+acc_inc_log=/tmp/acc_inc.log
+pro_inc_log=/tmp/pro_inc.log
+log_path=/data/appdir/java/logs/kinton-api/statistics/
+[ ! -d $log_path ] && echo "no exit logs path $log_path,Please Check it!" && exit -1
+cd $log_path
+
+#统计计算
+account_all()
+{
+pv=$(wc -l ${date1}|tail -n 1|awk '{print $1}')
+uv=$(awk -F']' '{print $3}' ${date1}|awk -F'['  '$2!="" {print $2}'|sort|uniq |wc -l)
+profile=$(awk -F']' '{print $4}' ${date1}|awk -F'['  '$2!="" {print $2}'|sort|uniq |wc -l)
+uv_incr=$(awk -F']' '$9="[1" {print $3,$9}' ${date1}|awk -F'[' '$2!=" " {print $2,$3}'|sort|uniq|wc -l)
+profile_incr=$(awk -F']' '{print $4,$10}' ${date1}|awk -F'[' '$3=="1" {print $2,$3}'|awk -F" " '$1!="" {print $1}'|uniq|wc -l)
+
+awk -F']' '{print $3,$9}' ${date2}|awk -F'[' '$3=="1" {print $2,$3}'|awk '$1!="" {print $1}'|sort|uniq > ${acc_inc_log}
+awk -F']' '{print $3}' ${date1}|awk -F'['  '$2!="" {print $2}'|sort|uniq >> ${acc_inc_log}
+account_retention=$(awk '{++S[$NF]} END {for (a in S)  {print S[a], a}}' ${acc_inc_log}|awk '$1>1 {print $1,$2}'|wc -l)
+
+awk -F']' '{print $4,$10}' ${date2}|awk -F'['  '$2!="  " {print $2 $3}'|awk -F" " '$2==1 {print $1}'|sort|uniq > ${pro_inc_log}
+awk -F']' '{print $4}' ${date1}|awk -F'['  '$2!="" {print $2 }'|sort|uniq >> ${pro_inc_log}
+profile_retention=$(awk '{++S[$NF]} END {for (a in S)  {print S[a], a}}' ${pro_inc_log}|awk '$1>1 {print $1,$2}'|wc -l)
+
+
+frequency_sql=$(awk -F']' '{print $2}'  ${date1}|awk -F'[' '{print $2}'|sort |uniq -c|sort -nr|awk 'date0=strftime("%Y%m%d",systime()-'$n'*24*3600) {print "insert '${frequency_table}' (date, count,attribute, type) value ("date0" ,"$1" ,\""$2"\", 1);"}')
+
+summary_sql="insert into ${summary_table} (date, pv, uv, profile, uv_incr,profile_incr,account_retention,profile_retention) values(${insert_date}, ${pv}, ${uv}, ${profile}, ${uv_incr}, ${profile_incr},${account_retention}, ${profile_retention});"
+}
+
+
+insert_mysql()
+{
+account_all
+$MYSQL1 -e "$frequency_sql"
+$MYSQL1 -e "$summary_sql"
+}
+
+
+
+
+case $1 in
+day)
+        date1=$(date -d '1 days ago' +%Y%m%d).log
+        date2=$(date -d '2 days ago' +%Y%m%d).log
+        n=1
+        insert_date=$(date -d '1 days ago' +%Y%m%d)
+        summary_table=summary
+        frequency_table=frequency
+        insert_mysql
+        ;;
+week)
+        date1=$(for i in {1..7};do eval d$i=$(/bin/date -d "$i days ago" +%Y%m%d) && echo $[d$i].log;done)
+        date2=$(for i in {8..14};do eval d$i=$(/bin/date -d "$i days ago" +%Y%m%d) && echo $[d$i].log;done)
+        n=7
+        insert_date=$(date -d '7 days ago' +%Y%m%d)
+        summary_table=summary_week
+        frequency_table=frequency_week
+        insert_mysql
+        ;;
+month)
+        date1=$(date +%Y%m --date="-1 month")*.log
+        date2=$(date +%Y%m --date="-2 month")*.log
+        n=$(date +%e)
+	insert_date=$(date --date="$(date +%e) days ago" '+%Y%m%d') 
+        summary_table=summary_month
+        frequency_table=frequency_month
+        insert_mysql
+        ;;
+esac
+
