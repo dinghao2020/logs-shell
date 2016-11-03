fa32ad992d81ea63f1134a1c7e5949906d949d0e
dinghao <18001326539@163.com>
2016-11-03 18:47:13 +0800

    多个文件夹下的文件同时统计

--- dirs_logs_acc01.sh
+++ dirs_logs_acc01.sh
@@ -1,12 +1,11 @@
 #!/bin/bash 
 
-which mysql
-
+[ -z $1 ] && echo "Please input parameter,eg 'day or week or month'" && exit 
+[ -z $(command -v mysql) ] && echo "mysql_client no exit in Path,Please Check it" && exit
 #MYSQL1="mysql -hmysql.cim.in -uom -pcim120_CIM120 om --default-character-set=utf8 -A -N"
-MYSQL1="mysql -h192.168.3.102 -udh -pgitLgit dinghaot1 --default-character-set=utf8 -A -N"
+MYSQL1="mysql -h192.168.3.102 -udh -pgitgit dinghaot1 --default-character-set=utf8 -A -N"
 acc_inc_log=/tmp/acc_inc.log
 pro_inc_log=/tmp/pro_inc.log
-#log_path=/data/appdir/java/logs/kinton-api/statistics/
 log_path=/home/dh/log_test/
 [ ! -d $log_path ] && echo "no exit logs path $log_path,Please Check it!" && exit -1
 cd $log_path
@@ -29,7 +28,9 @@ awk -F']' '{print $4}' ${date1}|awk -F'['  '$2!="" {print $2 }'|sort|uniq >> ${p
 profile_retention=$(awk '{++S[$NF]} END {for (a in S)  {print S[a], a}}' ${pro_inc_log}|awk '$1>1 {print $1,$2}'|wc -l)
 
 
-frequency_sql=$(awk -F']' '{print $2}'  ${date1}|awk -F'[' '{print $2}'|sort |uniq -c|sort -nr|awk 'date0=strftime("%Y%m%d",systime()-'$n'*24*3600) {print "insert '${frequency_table}' (date, count,attribute, type) value ("date0" ,"$1" ,\""$2"\", 1);"}')
+echo $n 
+sleep 2
+frequency_sql=$(awk -F']' '{print $2}'  ${date1}|awk -F'[' '{print $2}'|sort |uniq -c|sort -nr|awk 'date0=strftime("%Y%m%d",systime()-$n*24*3600) {print "insert '${frequency_table}' (date, count,attribute, type) value ("date0" ,"$1" ,\""$2"\", 1);"}')
 
 summary_sql="insert into ${summary_table} (date, pv, uv, profile, uv_incr,profile_incr,account_retention,profile_retention) values(${insert_date}, ${pv}, ${uv}, ${profile}, ${uv_incr}, ${profile_incr},${account_retention}, ${profile_retention});"
 }
@@ -43,12 +44,15 @@ $MYSQL1 -e "$summary_sql"
 }
 
 
+logs_dir=(A1 B1)
 
 
 case $1 in
 day)
-        date1=$(date -d '1 days ago' +%Y%m%d).log
-        date2=$(date -d '2 days ago' +%Y%m%d).log
+        dt1=$(date -d '1 days ago' +%Y%m%d).log
+        date1=$(for i in ${logs_dir[@]};do  for j in ${dt1}; do echo $i/$j;done;done)
+        dt2=$(date -d '2 days ago' +%Y%m%d).log
+        date2=$(for i in ${logs_dir[@]};do  for j in ${dt2}; do echo $i/$j;done;done)
         n=1
         insert_date=$(date -d '1 days ago' +%Y%m%d)
         summary_table=summary
@@ -56,8 +60,10 @@ day)
         insert_mysql
         ;;
 week)
-        date1=$(for i in {1..7};do eval d$i=$(/bin/date -d "$i days ago" +%Y%m%d) && echo $[d$i].log;done)
-        date2=$(for i in {8..14};do eval d$i=$(/bin/date -d "$i days ago" +%Y%m%d) && echo $[d$i].log;done)
+        dt1=$(for i in {1..7};do eval d$i=$(/bin/date -d "$i days ago" +%Y%m%d) && echo $[d$i].log;done)
+        date1=$(for i in ${logs_dir[@]};do  for j in ${dt1}; do echo $i/$j;done;done)
+        dt2=$(for i in {8..14};do eval d$i=$(/bin/date -d "$i days ago" +%Y%m%d) && echo $[d$i].log;done)
+        date2=$(for i in ${logs_dir[@]};do  for j in ${dt2}; do echo $i/$j;done;done)
         n=7
         insert_date=$(date -d '7 days ago' +%Y%m%d)
         summary_table=summary_week
@@ -65,13 +71,17 @@ week)
         insert_mysql
         ;;
 month)
-        date1=$(date +%Y%m --date="-1 month")*.log
-        date2=$(date +%Y%m --date="-2 month")*.log
+        dt1=$(date +%Y%m --date="-1 month")*.log
+        date1=$(for i in ${logs_dir[@]};do  for j in ${dt1}; do echo $i/$j;done;done)
+        dt2=$(date +%Y%m --date="-2 month")*.log
+        date2=$(for i in ${logs_dir[@]};do  for j in ${dt2}; do echo $i/$j;done;done)
         n=$(date +%e)
-	insert_date=$(date --date="$(date +%e) days ago" '+%Y%m%d') 
+	    insert_date=$(date --date="$(date +%e) days ago" '+%Y%m%d') 
         summary_table=summary_month
         frequency_table=frequency_month
         insert_mysql
         ;;
+*)
+       echo "You input parameter error, Please input 'day' ,or 'week' , or 'month' "
 esac
 
